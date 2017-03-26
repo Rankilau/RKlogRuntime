609d36629b1a4da5c678ad184f093432b0e60993
rankilau <liuyu26@meituan.com>
2017-03-26 13:57:25 +0800

    起了一个标题

--- RKlogRuntime/AppDelegate.m
+++ RKlogRuntime/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"这是一个利用runtime埋点的小程序");
     // Override point for customization after application launch.
     return YES;
 }
