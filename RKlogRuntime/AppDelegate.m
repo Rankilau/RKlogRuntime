6732ce94a4ae795dcf6ced8d7392d26c50d14329
rankilau <liuyu26@meituan.com>
2017-03-26 12:59:20 +0800

    fixbug

--- RKlogRuntime/AppDelegate.m
+++ RKlogRuntime/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"octocatLau 帮你修复了一个bug");
     return YES;
 }
 
