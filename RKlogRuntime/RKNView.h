95d9843a11be0b576bab1ea6a8017280fcebfcaa
RankiLau <rankiliu@163.com>
2017-04-15 19:28:11 +0800

    添加N文件

---
+++ RKlogRuntime/RKNView.h
@@ -0,0 +1,13 @@
+//
+//  RKNView.h
+//  RKlogRuntime
+//
+//  Created by rankilau on 2017/4/15.
+//  Copyright © 2017年 rankilau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKNView : UIView
+
+@end
---
+++ RKlogRuntime/RKNView.m
@@ -0,0 +1,21 @@
+//
+//  RKNView.m
+//  RKlogRuntime
+//
+//  Created by rankilau on 2017/4/15.
+//  Copyright © 2017年 rankilau. All rights reserved.
+//
+
+#import "RKNView.h"
+
+@implementation RKNView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
